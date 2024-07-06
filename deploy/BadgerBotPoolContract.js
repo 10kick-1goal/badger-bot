5c747a3b70c3e27f8b009c226c6779a47765ed0e
10kick-1goal <kick.3top@gmail.com>
2024-07-05 19:24:40 -0700

    feat: update deploy environments

--- deploy/BadgerBotPoolContract.js
+++ deploy/BadgerBotPoolContract.js
@@ -3,8 +3,13 @@ const { verify } = require("../utils/verify")
 
 async function main() {
     console.log("----------------------------------------------------")
+    const botAddress = "0x840F795E46277DdC58eB6c3ad8deCE2221FAdF4E";
+    const wethSepoliaAddress = "0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14";
+    const wethEthereumAddress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+    const initBaseURI = process.env.TEST_BASE_URI;
+
     const Contract = await ethers.getContractFactory('BadgerBotPool');
-    const contract = await Contract.deploy();
+    const contract = await Contract.deploy(botAddress, wethSepoliaAddress, initBaseURI);
 
     console.log("Contract address: ", contract.address);
 
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,7 @@
 require("@nomiclabs/hardhat-waffle")
-require("hardhat-gas-reporter")
 require("@nomiclabs/hardhat-etherscan")
 require("dotenv").config()
-require("solidity-coverage")
-require("hardhat-deploy")
+
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 /**
@@ -14,7 +12,7 @@ const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
 const GOERLI_RPC_URL =
     process.env.GOERLI_RPC_URL || "https://eth-goerli.alchemyapi.io/v2/your-api-key"
 const SEPOLIA_RPC_URL =
-    process.env.SEPOLIA_RPC_URL || "https://eth-goerli.alchemyapi.io/v2/your-api-key"
+    process.env.SEPOLIA_RPC_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY || ""
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
 
@@ -48,37 +46,55 @@ module.exports = {
         compilers: [
             {
                 version: "0.6.0",
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 200,
+                    },
+                },
             },
             {
                 version: "0.6.2",
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 200,
+                    },
+                },
             },
             {
                 version: "0.7.0",
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 200,
+                    },
+                },
             },
             {
                 version: "0.8.0",
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 200,
+                    },
+                },
             },
             {
                 version: "0.8.20",
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 200,
+                    },
+                },
             },
         ],
     },
     etherscan: {
         apiKey: ETHERSCAN_API_KEY,
     },
-    gasReporter: {
-        enabled: true,
-        currency: "USD",
-        outputFile: "gas-report.txt",
-        noColors: true,
-        // coinmarketcap: COINMARKETCAP_API_KEY,
-    },
-    namedAccounts: {
-        deployer: {
-            default: 0, // here this will by default take the first account as deployer
-            1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
-        },
-    },
+    
     mocha: {
         timeout: 200000, // 200 seconds max for running tests
     },
