aad7cf1e87b435b5ca9f9c3748a53498ed90ff1a
10kick-1goal <kick.3top@gmail.com>
2024-07-10 05:03:03 -0700

    chore: delete weth address from params of staking contract deploy function

--- deploy/BadgerBotStakingContract.js
+++ deploy/BadgerBotStakingContract.js
@@ -4,11 +4,10 @@ const { verify } = require("../utils/verify")
 async function main() {
     console.log("----------------------------------------------------")
     const nftCollectionAddress = "0x6391A65821dd53E6557946d7e4514205e1bcBE01";
-    const wethAddress = "0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14"; 
     const teamAddress = "0x6360A1E7dFe205397d7EF463cb28f16Fbdaa2D24";
 
     const Contract = await ethers.getContractFactory('BadgerBotStakingContract');
-    const contract = await Contract.deploy(nftCollectionAddress, wethAddress, teamAddress);
+    const contract = await Contract.deploy(nftCollectionAddress, teamAddress);
 
     console.log("Contract address: ", contract.address);
 
